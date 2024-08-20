c0d45eebd26a6734398a26d7c1f4dc91aa4279aa
cs.benliu <cs.benliu@gmail.com>
2024-08-19 22:29:55 -0400

    created getQuote and getLastQuote

--- othercode.go
+++ othercode.go
@@ -33,3 +33,72 @@ for iter.Next() {
 	fmt.Println(timeValue.Format(time.RFC3339))
 
 }
+
+
+
+// websocket 
+
+c, err := polygonws.New(polygonws.Config{
+	APIKey: "ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm",
+	Feed:   polygonws.RealTime,
+	Market: polygonws.Stocks,
+})
+if err != nil {
+
+	log.Fatal(err)
+}
+defer c.Close()
+if err := c.Connect(); err != nil {
+	log.Fatal(err)
+	return
+}
+if err := c.Subscribe(polygonws.StocksTrades, "SMCI"); err != nil {
+	log.Fatal(err)
+}
+for {
+	select {
+	case err := <-c.Error(): // check for any fatal errors (e.g. auth failed)
+		log.Fatal(err)
+	case out, more := <-c.Output(): // read the next data message
+		if !more {
+			return
+		}
+
+		switch out.(type) {
+		case models.EquityQuote:
+			log.Print(out) // do something with the agg
+		case models.EquityTrade:
+			log.Print(out) // do something with the trade
+		}
+	}
+}
+
+// all tickers snapshot
+
+package main
+
+import (
+	"context"
+	"log"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func main() {
+	c := polygon.New("ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm")
+
+	params := models.GetAllTickersSnapshotParams{
+
+		Locale:     models.US,
+		MarketType: models.Stocks,
+	}.WithTickers("")
+	res, err := c.GetAllTickersSnapshot(context.Background(), params)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+	log.Print(res)
+
+}
+
