113b30c3e3641c784110f7edc60f83a0c3493a17
cs.benliu <cs.benliu@gmail.com>
2024-08-19 20:23:12 -0400

    changed othercode and testing

--- testing.go
+++ testing.go
@@ -1,32 +1,45 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
+	"log"
 
-	polygon "github.com/polygon-io/client-go/rest"
-	"github.com/polygon-io/client-go/rest/models"
+	polygonws "github.com/polygon-io/client-go/websocket"
+	"github.com/polygon-io/client-go/websocket/models"
 )
 
 func main() {
+	c, err := polygonws.New(polygonws.Config{
+		APIKey: "ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm",
+		Feed:   polygonws.RealTime,
+		Market: polygonws.Stocks,
+	})
+	if err != nil {
 
-	// init client
-	c := polygon.New("ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm")
-	fmt.Print("Hello")
-	parameters := &models.ListAggsParams{
-		Ticker:     "COIN",
-		Multiplier: 5,
-		Timespan:   "minute",
-		To:         models.Millis(time.Date(2024, 8, 13, 0, 0, 0, 0, time.UTC)),
-		From:       models.Millis(time.Date(2024, 8, 12, 0, 0, 0, 0, time.UTC)),
+		log.Fatal(err)
 	}
-
-	iter := c.ListAggs(context.Background(), parameters)
-
-	for iter.Next() {
-
-		fmt.Print(iter.Item())
+	defer c.Close()
+	if err := c.Connect(); err != nil {
+		log.Fatal(err)
+		return
 	}
+	if err := c.Subscribe(polygonws.StocksTrades, "SMCI"); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		select {
+		case err := <-c.Error(): // check for any fatal errors (e.g. auth failed)
+			log.Fatal(err)
+		case out, more := <-c.Output(): // read the next data message
+			if !more {
+				return
+			}
 
+			switch out.(type) {
+			case models.EquityQuote:
+				log.Print(out) // do something with the agg
+			case models.EquityTrade:
+				log.Print(out) // do something with the trade
+			}
+		}
+	}
 }
