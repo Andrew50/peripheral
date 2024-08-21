1714bebe8426eb99b94f916f201ac13e70380ee1
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-08-21 17:52:01 -0400

    idk

---
+++ backend/tasks/annotate.go
@@ -0,0 +1,34 @@
+package tasks
+
+import (
+)
+
+
+
+type SetAnnotationArgs struct {
+    AnnotationId int `json:"a1"`
+    Entry string `json:"a2"`
+}
+
+func SetAnnotation(conn *data.Conn, rawArgs json.RawMessage) (interface{}, error) {
+    var args SetAnnotationArgs
+    if err := json.Unmarshal(rawArgs, &args); err != nil {
+        return nil, fmt.Errorf("getAnnotations invalid args: %v", err)
+    }
+
+    cmdTag, err := conn.DB.Exec(context.Background(), "UPDATE annotations SET entry = $1, completed = true WHERE annotation_id = $2 AND user_id = $3", args.Entry, args.Annotation_id, user_id)
+    if err != nil {
+        return nil, fmt.Errorf("SetAnnotation execution failed: %v", err)
+    }
+
+    // Check if any rows were affected, this ensures the annotation_id existed
+    if cmdTag.RowsAffected() == 0 {
+        return nil, fmt.Errorf("SetAnnotation no annotation found with the provided annotation_id and user_id")
+    }
+
+    return "success", nil
+}
+
+
+
+
