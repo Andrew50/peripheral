7a36b2709ac434972bc64bc5a2e8f065189c5d49
server <billingsandrewjohn@gmail.com>
2024-08-21 15:04:42 -0400

    setting up instances, figuring out how to store in db, manage in backend with go structs or whatever, manage frontend, and display, work in progress

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,39 @@ CREATE TABLE users (
     settings JSONB DEFAULT '{}'
 );
 CREATE INDEX idx_users ON users (username, password);
+
+CREATE TABLE instances (
+    instance_id serial PRIMARY KEY,
+    user_id serial references users(user_id),
+    security_id varchar(100),
+    timestamp timestamp
+);
+
+create table training_instances (
+    instance_id serial references instances(instances_id),
+    setup_id serial references setups(setup_id),
+    label boolean
+)
+
+
+CREATE TABLE annotations (
+    instance_id serial references instances(instance_id),
+    timeframe , 
+    annotation text
+);
+
+
+CREATE TABLE journals (
+    journal_id serial primary key,
+    user_id serial references users(user_id),
+    entry text
+);
+
+
+CREATE TABLE journal_instances (
+    journal_id SERIAL REFERENCES journals(journal_id) ON DELETE CASCADE,
+    instance_id SERIAL REFERENCES instances(instance_id) ON DELETE CASCADE,
+    character pos
+    PRIMARY KEY (journal_id, instance_id)
+);
+
