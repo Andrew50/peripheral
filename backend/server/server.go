b9ad96eb9c8c3329a41c2d4da3bb6186e93a373a
server <billingsandrewjohn@gmail.com>
2024-08-21 17:52:01 -0400

    idk

--- backend/server/server.go
+++ backend/server/server.go
@@ -2,27 +2,28 @@ package server
 
 import (
     "fmt"
-    //"api/tasks"
+    "api/tasks"
     "api/data"
     "encoding/json"
     "log"
     "net/http"
+)
 /*    "context"
     "github.com/jackc/pgx/v4/pgxpool"
     "github.com/go-redis/redis/v8"
     */
-)
 
 
 var publicFunc = map[string]func(*data.Conn, json.RawMessage) (interface{}, error) {
-    "signup": Signup,
-    "login": Login,
+    "annotate": tasks.Annotate,
+  //  "signup": Signup,
+   // "login": Login,
 }
 
-var privateFunc = map[string]func(*data.Conn, int, json.RawMessage) (interface{}, error){
+/*var privateFunc = map[string]func(*data.Conn, int, json.RawMessage) (interface{}, error){
     /*"getJournal": tasks.GetJournal,
     "setJournal": tasks.SetJournal,*/
-}
+//}*/
 
 type Request struct {
     Function string `json:"func"`
@@ -76,7 +77,7 @@ func public_handler(conn *data.Conn) http.HandlerFunc {
     }
 }
 
-func private_handler(conn *data.Conn) http.HandlerFunc {
+/*func private_handler(conn *data.Conn) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         addCORSHeaders(w)
         if r.Method != "POST" {
@@ -109,12 +110,12 @@ func private_handler(conn *data.Conn) http.HandlerFunc {
             return
         }
     }
-}
+}*/
 
 func StartServer() {
     conn := data.InitConn()
-    http.HandleFunc("/public", public_handler(conn))
-    http.HandleFunc("/private", private_handler(conn))
+    http.HandleFunc("/", public_handler(conn))
+  //  http.HandleFunc("/private", private_handler(conn))
     fmt.Println("Server running on port 5057")
     if err := http.ListenAndServe(":5057",nil); err != nil {
         log.Fatal(err)
