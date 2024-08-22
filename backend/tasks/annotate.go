c23b23ac3e72edbafbe922d317271310ce2841a2
cs.benliu <cs.benliu@gmail.com>
2024-08-22 15:25:13 -0400

    docker and server

--- backend/tasks/annotate.go
+++ backend/tasks/annotate.go
@@ -1,34 +1,32 @@
 package tasks
 
 import (
+	"api/data"
+	"context"
+	"encoding/json"
+	"fmt"
 )
 
-
-
 type SetAnnotationArgs struct {
-    AnnotationId int `json:"a1"`
-    Entry string `json:"a2"`
+	AnnotationId int    `json:"a1"`
+	Entry        string `json:"a2"`
 }
 
 func SetAnnotation(conn *data.Conn, rawArgs json.RawMessage) (interface{}, error) {
-    var args SetAnnotationArgs
-    if err := json.Unmarshal(rawArgs, &args); err != nil {
-        return nil, fmt.Errorf("getAnnotations invalid args: %v", err)
-    }
+	var args SetAnnotationArgs
+	if err := json.Unmarshal(rawArgs, &args); err != nil {
+		return nil, fmt.Errorf("getAnnotations invalid args: %v", err)
+	}
 
-    cmdTag, err := conn.DB.Exec(context.Background(), "UPDATE annotations SET entry = $1, completed = true WHERE annotation_id = $2 AND user_id = $3", args.Entry, args.Annotation_id, user_id)
-    if err != nil {
-        return nil, fmt.Errorf("SetAnnotation execution failed: %v", err)
-    }
+	cmdTag, err := conn.DB.Exec(context.Background(), "UPDATE annotations SET entry = $1, completed = true WHERE annotation_id = $2 AND user_id = $3", args.Entry, args.Annotation_id, user_id)
+	if err != nil {
+		return nil, fmt.Errorf("SetAnnotation execution failed: %v", err)
+	}
 
-    // Check if any rows were affected, this ensures the annotation_id existed
-    if cmdTag.RowsAffected() == 0 {
-        return nil, fmt.Errorf("SetAnnotation no annotation found with the provided annotation_id and user_id")
-    }
+	// Check if any rows were affected, this ensures the annotation_id existed
+	if cmdTag.RowsAffected() == 0 {
+		return nil, fmt.Errorf("SetAnnotation no annotation found with the provided annotation_id and user_id")
+	}
 
-    return "success", nil
+	return "success", nil
 }
-
-
-
-
