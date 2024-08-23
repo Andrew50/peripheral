e7aaf0715038e22c0dae7b71f27dbcac0a438e0c
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-08-23 14:53:48 -0400

    idek

--- backend/tasks/instance.go
+++ backend/tasks/instance.go
@@ -17,7 +17,7 @@ func NewInstance (conn *data.Conn, userId int, rawArgs json.RawMessage) (interfa
     if err := json.Unmarshal(rawArgs, &args); err != nil {
         return nil, fmt.Errorf("NewInstance invalid args: %v", err)
     }
-    _, err := conn.DB.Exec(context.Background(), "insert into annotations security_id = $1, timestamp = $2, user_id = $3", args.SecurityId, args.Timestamp, userId)
+    _, err := conn.DB.Exec(context.Background(), "insert into instances (security_id, timestamp, user_id) values ($1, $2, $3) ", args.SecurityId, args.Timestamp, userId)
     if err != nil {
         return nil, fmt.Errorf("NewIstance execution failed: %v", err)
     }
