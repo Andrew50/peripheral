33e614deafa019e3efcc84fb013fd0b13cf6f9de
server <billingsandrewjohn@gmail.com>
2024-08-23 16:30:26 -0400

    working on instances and annotations sepiecally the frontend data handling p[art

--- backend/tasks/instance.go
+++ backend/tasks/instance.go
@@ -9,7 +9,7 @@ import (
 
 type NewInstanceArgs struct {
     SecurityId string `json:"a1"`
-    Timestamp int `json:"a2"`
+    Timestamp string `json:"a2"`
 }
 
 func NewInstance (conn *data.Conn, userId int, rawArgs json.RawMessage) (interface{}, error) {
