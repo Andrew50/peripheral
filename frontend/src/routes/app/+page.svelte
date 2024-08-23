00f938adbb5116cdf79f81a7d4b2af4971b64ea4
server <billingsandrewjohn@gmail.com>
2024-08-23 13:56:21 -0400

    finnally starting instance stuff, frontend text box adds new isntance to db

--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,13 +1,41 @@
 <!-- app/+page.svelte -->
 <script lang="ts">
-    import {auth_data} from '../../store.js'
+    import {auth_data, request} from '../../store.js'
     import { goto } from '$app/navigation';
     import { browser } from '$app/environment';
-    let entry: string
+    let securityId: string;
+    let timestamp: number;
 
     $: if ($auth_data == null && browser) {
         goto('/login');
     }
+    function submit (): void {
+        if (securityId && timestamp) {
+            request(null, true, "NewInstance", securityId, timestamp)
+        }
+    }
+        
 </script>
+<h1> new instance </h1>
+<div class="form" >
+<div>
+<input bind:value={securityId}/>
+</div>
+<div>
+<input type="number" bind:value={timestamp}/>
+</div>
+<div>
+<button on:click={submit}> enter </button>
+</div>
+</div>
+
+
+<style>
+
+
+
+</style>
+
+
 
 
