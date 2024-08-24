068b6a94bbc2ce063cc87b7ae0f597692bb57c57
bliuak <cs.benliu@gmail.com>
2024-08-23 20:38:12 -0400

    fod

--- backend/tasks/instance.go
+++ backend/tasks/instance.go
@@ -8,39 +8,37 @@ import (
 )
 
 type GetCikArgs struct {
-	Ticker string `json:"a1"`
+	TickerString string `json:"a1"`
 }
 
 func GetCik(conn *data.Conn, userId int, rawArgs json.RawMessage) (interface{}, error) {
 	var args GetCikArgs
-
-	if err := json.Unmarshal(rawArgs, &args); err != nil {
+	err := json.Unmarshal(rawArgs, &args)
+	if err != nil {
 		return nil, fmt.Errorf("GetCik invalid args: %v", err)
 	}
-	cik, err := data.GetCIK(conn.Polygon, args.Ticker)
-	if err != nil {
-		return nil, err
-	}
+	cik := data.GetCIK(conn.Polygon, args.TickerString)
 	return cik, err
 
 }
 
 type NewInstanceArgs struct {
-	Cik       int    `json:"a1"`
+	Cik       string `json:"a1"`
 	Timestamp string `json:"a2"`
 }
 
 func NewInstance(conn *data.Conn, userId int, rawArgs json.RawMessage) (interface{}, error) {
 	var args NewInstanceArgs
-
+	fmt.Print("NewInstance hit")
 	if err := json.Unmarshal(rawArgs, &args); err != nil {
 		return nil, fmt.Errorf("NewInstance invalid args: %v", err)
 	}
-	var rowID int64
-	err := conn.DB.QueryRow(context.Background(), "insert into instances (cik, timestamp, user_id) values ($1, $2, $3) ", args.Cik, args.Timestamp, userId).Scan(&rowID)
+	var instanceID int
+	err := conn.DB.QueryRow(context.Background(), "insert into instances (user_id, cik, timestamp) values ($1, $2, $3) RETURNING instance_id", userId, args.Cik, args.Timestamp).Scan(&instanceID)
 	if err != nil {
 		return nil, fmt.Errorf("NewInstance execution failed: %v", err)
 	}
+	fmt.Print(instanceID)
 
-	return rowID, err
+	return instanceID, err
 }
