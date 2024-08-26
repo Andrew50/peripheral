0f59f2254c3805a4eb2580712c6dd5676d20214e
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-08-26 12:47:11 -0400

    girnding out annotation functionality

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,18 +2,20 @@ import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import { get } from 'svelte/store';
 import { goto } from "$app/navigation";
+import { onMount } from 'svelte';
+
 export let menuLeftPos = writable(400);
 export let annotateData = writable([]);
 export let journalData = writable([]);
 export let screener_data = writable([]);
 export let chartQuery = writable([]);
 export let match_data = writable([[], [], []]);
-export let auth_data = writable("");
 export let setups_list = writable([]);
 export let currentEntry = writable("");
 export let watchlist_data = writable({});
 export let settings = writable({});
 export const focus = writable(null);
+
 let base_url: string;
 
 if (typeof window !== 'undefined') {
@@ -29,14 +31,10 @@ if (typeof window !== 'undefined') {
 }
 
 export function logout() {
-    auth_data.set("");
-    goto('/');
+    sessionStorage.setItem("authToken","")
+    goto('/login');
 }
 
-/*export async function publicRequest(){
-    url = `${base_url}/public`;
-    headers = { 'Content-Type': 'application/json' };
-}*/
 export async function publicRequest<T>(func: string, args: any, error: Writable<string>): Promise<T> {
     const payload = JSON.stringify({
         func: func,
@@ -60,31 +58,30 @@ export async function publicRequest<T>(func: string, args: any, error: Writable<
 }
 
 export async function privateRequest<T>(func: string, args: any, error: Writable<string>): Promise<T> {
-    const authToken = get(auth_data)
+    const authToken = sessionStorage.getItem("authToken")
     const headers = {
         'Content-Type': 'application/json',
         ...(authToken ? { 'Authorization': authToken} : {}),
     };
-    /*const filteredHeaders = Object.fromEntries(
-        Object.entries(headers).filter(([_, value]) => value !== undefined)
-    );*/
-    const payload = JSON.stringify({
+    const payload = {
         func: func,
         args: args
-    })
+    }
     const response = await fetch(`${base_url}/private`, {
         method: 'POST',
         headers: headers,
-        body: payload});
+        body: JSON.stringify(payload)
+    });
     if (response.ok){
         error.set("")
         const result = await response.json() as T
         console.log("payload: ",payload, "result: ", result)
+        console.log("gooddddd")
         return result;
     }else{
         const errorMessage = await response.text()
         error.set(errorMessage);
-        console.error("payload: ",payload, "result: ", errorMessage)
+       console.error("payload: ",payload, "result: ", errorMessage)
         return Promise.reject();
     }
 }
