474d54941197debed3733db3cdb5bf8db71e1699
server <billingsandrewjohn@gmail.com>
2024-08-27 16:22:41 -0400

    more ebmedded isntacnes

--- frontend/src/routes/app/entry.svelte
+++ frontend/src/routes/app/entry.svelte
@@ -1,29 +1,45 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store'
-    let Quill;
-    let currentEntry = writable('')
     import { browser } from '$app/environment';
     import 'quill/dist/quill.snow.css';
-    let editorContainer;
-    let editor;
+    import type Quill from 'quill'
+    import type { DeltaStatic, Sources, DeltaOperation, EmbedBlot } from 'quill'
+
+    let Quill;
+    let editorContainer: HTMLElement | string;
+    let editor: Quill | undefined;
+    let ticker = "";
+    let timeframe = "";
+    let datetime = "";
+    let pm = false;
+    let insertInstanceVisible = false;
+
+    function insertInstance(): void {
+        editor.insertEmbed(-1, 'embeddedInstance', {ticker, timeframe, datetime, pm});
+    }
+
+    function embeddedInstanceClick(): void {
+        console.log("clicked embedded instance")
+    }
 
     onMount(() => {
         import('quill').then(QuillModule => {
             Quill = QuillModule.default;
             editor = new Quill(editorContainer, {
                 theme: 'snow',
+                placeholder: 'Entry ...',
                 modules: {
-                    toolbar: false // Explicitly disable the toolbar
+                    toolbar: false
                 }
             });
-            editor.on('text-change', (delta, oldDelta, source) => {
+            editor.on('text-change', (delta: DeltaStatic, _, source: Sources) => {
                 if (source === 'user' || source === 'api') {
                     let text = editor.getText();
-                    if (source === 'user') {
+                    /*if (source === 'user') {
                         currentEntry.set(text); 
-                    }
-                    delta.ops.forEach(op => {
+                    }*/
+                    delta.ops.forEach((op: DeltaOperation) => {
                         if (typeof op.insert === 'string') {
                             const regex = /\[([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\]]+)\]/g;
                             let match;
@@ -31,28 +47,23 @@
                                 const [_, ticker, interval, t, pm] = match;
                                 const index = match.index;
                                 editor.deleteText(index, match[0].length);
-                                editor.insertEmbed(index, 'chart', { ticker, interval, t, pm });
+                                editor.insertEmbed(index, 'embeddedInstance', { ticker, interval, t, pm });
                             }
                         }
                     });
                 }
             });
-            currentEntry.subscribe(value => {
-                if (editor.getText() !== value) {
-                    editor.setText(value); 
-                }
-            });
-            class ChartBlot extends Quill.import('blots/embed') {
-                static create(value) {
+            class ChartBlot extends (Quill.import('blots/embed') as typeof EmbedBlot) {
+                static create(value: any): HTMLElement {
                     let node = super.create();
                     node.setAttribute('type', 'button');
                     node.className = 'btn';
                     node.textContent = `${value.ticker}`; 
-                    node.onclick = () => chartQuery.set([value.ticker, value.interval, value.t, value.pm]);
+                    node.onclick = embeddedInstanceClick//() => chartQuery.set([value.ticker, value.interval, value.t, value.pm]);
                     return node;
                 }
 
-                static value(node) {
+                static value(node: HTMLElement) {
                     return {
                         ticker: node.dataset.ticker,
                         interval: node.dataset.interval,
@@ -61,13 +72,33 @@
                     };
                 }
             }
-            ChartBlot.blotName = 'chart';
+            ChartBlot.blotName = 'embeddedInstance';
             ChartBlot.tagName = 'button';
-            Quill.register('formats/chart', ChartBlot);
+            Quill.register('formats/embeddedInstance', ChartBlot);
         })
     });
 </script>
 <div bind:this={editorContainer}></div>
+<button on:click={() => {insertInstanceVisible = true} }> Insert Instance </button>
+{#if insertInstanceVisible}
+    <div class="form" >
+        <div>
+            <input bind:value={ticker} placeholder="ticker"/>
+        </div>
+        <div>
+            <input type="date" bind:value={datetime} placeholder="datetime"/>
+        </div>
+        <div>
+            <input  bind:value={timeframe} placeholder="timeframe"/>
+        </div>
+        <div>
+            <button on:click={insertInstance}> enter </button>
+        </div>
+    </div>
+{/if}
+<div>
+    <button on:click={() => {console.log(editor.getContents())}}> save </button>
+</div>
 <style>
   .ql-container {
     /*height: 200px;*/
