e50803b3887d4a25451c778e4d4b8cdf0de92754
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-08-26 21:37:26 -0400

    fixed db

--- db/init.sql
+++ db/init.sql
@@ -5,14 +5,14 @@ CREATE TABLE users (
     password VARCHAR(100) NOT NULL
 );
 CREATE INDEX idxUsers ON users (username, password);
-CREATE TABLE securities {
-    cik varchar(10) not null, 
+CREATE TABLE securities (
+    cik varchar(10) primary key, 
     ticker varchar(8) not null
-}
+);
 create index idxCik on securities (cik);
 create table setups (
     setupId serial primary key,
-    userId int references user(userId) on delete cascade, 
+    userId int references users(userId) on delete cascade, 
     name varchar(50) not null,
     timeframe varchar(10) not null
 );
@@ -26,7 +26,7 @@ CREATE TABLE instances (
 create index idxInstances on instances (cik, timestamp);
 create table samples (
     sampleId serial references instances(instanceId) on delete cascade,
-    instanceId integer references instances(instandId) on delete cascade,
+    instanceId integer references instances(instanceId) on delete cascade,
     setupId serial references setups(setupId) on delete cascade,
     label boolean default null,
     unique (instanceId, setupId)
@@ -35,8 +35,7 @@ CREATE TABLE annotations (
     annotationId serial primary key,
     instanceId serial references instances(instanceId) on delete cascade,
     entry text not null default '', 
-    completed boolean not null default false,
-    unique (instanceId, timeframe)
+    completed boolean not null default false
 );
 CREATE TABLE journals (
     journalId serial primary key,
