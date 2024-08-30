bf4ed287d06f44686806807cd7739b7c828e330d
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-08-30 18:23:55 -0400

    ticker overlay

--- frontend/src/routes/app/chart.svelte
+++ frontend/src/routes/app/chart.svelte
@@ -1,13 +1,11 @@
+<!-- chart.svelte-->
 <script lang="ts">
 import { createChart, ColorType} from 'lightweight-charts';
-import {privateRequest} from '../../store';
+import {privateRequest, chartQuery, instanceInputVisible} from '../../store';
 import type {IChartApi, ISeriesApi, CandlestickData, Time, WhitespaceData, CandlestickSeriesOptions, DeepPartial, CandlestickStyleOptions, SeriesOptionsCommon, MouseEventParams, UTCTimestamp} from 'lightweight-charts';
 import { onMount, onDestroy } from 'svelte';
-
 let mainChart: IChartApi;
 let mainChartCandleSeries: ISeriesApi<"Candlestick", Time, WhitespaceData<Time> | CandlestickData<Time>, CandlestickSeriesOptions, DeepPartial<CandlestickStyleOptions & SeriesOptionsCommon>>
-let currentTicker: string;
-let currentTimeframe: string; 
 let latestCrosshairPositionTime: Time;
 // Right Click context menu variables 
 let showMenu = false; 
@@ -27,18 +25,9 @@ function initializeChart()  {
     const chartContainer = document.getElementById('chart_container');
     if (chartContainer) {
         chartContainer.addEventListener('keydown', event => {
-            if (/^[a-zA-Z]$/.test(event.key)) {
-                event.preventDefault();
-                currentTicker += event.key.toUpperCase();
-                // Perform action for any letter key
-            } else if (event.key === 'Backspace') {
-                event.preventDefault();
-                currentTicker = currentTicker.slice(0, -1);
-            } else if (event.key === 'Enter') {
-                event.preventDefault();
-                updateChart(mainChart);
+            if (/^[a-zA-Z0-9]$/.test(event.key.toLowerCase())) {
+                instanceInputVisible.set(true);
             }
-            
          });
         mainChart = createChart(chartContainer, chartOptions);
 
@@ -53,7 +42,6 @@ function initializeChart()  {
             {time: "2024-08-15", open: 1, high: 2, low: 0.5, close: 2},
             {time: "2024-08-16", open: 1, high: 2, low: 0.5, close: 2},
         ]
-
         mainChartCandleSeries.setData(candleData);
         mainChart.subscribeCrosshairMove(crosshairMoveEvent);
         mainChart.timeScale().fitContent();
@@ -74,38 +62,6 @@ function crosshairMoveEvent(param: MouseEventParams) {
     latestCrosshairPositionTime = bar.time 
 
 }
-function updateChart(chart: IChartApi) {
-    console.log(currentTicker)
-    let barDataList: barData[] = []
-    privateRequest<barData[]>("getChartData", {ticker:currentTicker, timeframe:currentTimeframe})
-        .then((result: barData[]) => {
-            barDataList = result;
-
-            let newData = [];
-            for (let i =0; i < barDataList.length; i++) {
-                newData.push({
-                    time: barDataList[i].time, 
-                    open: barDataList[i].open, 
-                    high: barDataList[i].high, 
-                    low: barDataList[i].low,
-                    close: barDataList[i].close
-                });
-            }
-           
-            console.log(newData)
-            mainChart.removeSeries(mainChartCandleSeries)
-            mainChartCandleSeries = mainChart.addCandlestickSeries({
-                    upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
-                    wickUpColor: '#26a69a', wickDownColor: '#ef5350',
-                })
-            mainChartCandleSeries.setData(newData)
-            mainChart.timeScale().fitContent();
-        })
-        .catch((error: string) => {
-            console.error("Error fetching chart data:", error);
-        });
-
-}
 function chartRightClick(event: MouseEvent) {
     event.preventDefault();
     console.log("Chart right clicked")
@@ -115,20 +71,50 @@ function chartRightClick(event: MouseEvent) {
             left: `${event.clientX + 10}px`
         };
     showMenu = true;
-    
-
-
 }
 function closeRightClickMenu() {
     showMenu = false;
 }
+
 onMount(() => {
-    currentTicker = ""
+    chartQuery.subscribe((v:any) => {
+        privateRequest<barData[]>("getChartData", {security:v.securityId, timeframe:v.timeframe})
+        .then((result: barData[]) => {
+            if (Array.isArray(result)){
+                let barDataList: barData[] = []
+                barDataList = result;
+                let newData = [];
+                for (let i =0; i < barDataList.length; i++) {
+                    newData.push({
+                        time: barDataList[i].time, 
+                        open: barDataList[i].open, 
+                        high: barDataList[i].high, 
+                        low: barDataList[i].low,
+                        close: barDataList[i].close
+                    });
+                }
+                mainChart.removeSeries(mainChartCandleSeries)
+                mainChartCandleSeries = mainChart.addCandlestickSeries({
+                        upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+                        wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+                    })
+                mainChartCandleSeries.setData(newData)
+                mainChart.timeScale().fitContent();
+            }else{
+                console.log("39ffdw invalid bar data")
+            }
+        })
+        .catch((error: string) => {
+            console.error("Error fetching chart data:", error);
+        });
+    });
+
     initializeChart(); 
     const chartContainer = document.getElementById('chart_container');
     if (chartContainer) {
         chartContainer.addEventListener('contextmenu', chartRightClick)
     }
+//    document.addEventListener('click', closeRightClickMenu)
     return () => {
         if (chartContainer) {
                 chartContainer.removeEventListener('contextmenu', chartRightClick);
@@ -136,10 +122,9 @@ onMount(() => {
     };
 
 });
-document.addEventListener('click', closeRightClickMenu)
-onDestroy(() => {
-        document.removeEventListener('click', closeRightClickMenu)
-})
+//onDestroy(() => {
+ //       document.removeEventListener('click', closeRightClickMenu)
+//})
 </script>
 <style>
     #chart_container {
@@ -166,10 +151,6 @@ onDestroy(() => {
         background-color: #f0f0f0;
     }
 </style>
-<p> Lightweight Charts</p>
-<input bind:value={currentTicker} placeholder="ticker"/>
-<input bind:value={currentTimeframe} placeholder="timeframe"/>
-<button on:click={() => updateChart(mainChart)}>Get Data</button>
 <div id="chart_container" tabindex="0"></div>
 {#if showMenu}
     <div class="context-menu" style="top: {menuStyle.top}; left: {menuStyle.left};">
--- frontend/src/routes/app/instance.svelte
+++ frontend/src/routes/app/instance.svelte
@@ -1,67 +1,94 @@
+<!-- instance.svlete -->
 <script lang="ts">
-    import { privateRequest } from '../../store';
-    import { onMount } from 'svelte';
-    import type { Writable } from 'svelte/store';
-
-    let ticker = "";
-    let isOpen = false;
+    import { privateRequest, chartQuery , instanceInputVisible} from '../../store';
+    import { onMount, tick } from 'svelte';
+    import { get } from 'svelte/store';
+    import type {ChartQuery} from '../../store';
+    let inputString = "";
+    let inputType = "";
     let selectedSecurityIndex = 0;
-
+    let prevFocus: HTMLElement | null = null;
     interface Security {
         securityId: number;
         ticker: string;
         maxDate: string;
         name: string;
     }
-    export let securityId: Writable<number>;
-
     let securities: Security[] = [];
-    
+    instanceInputVisible.subscribe(async (v) => {
+        if ( v && typeof window !== 'undefined'){
+            await tick();
+            const element = document.getElementById("instanceInput");
+            if (element){
+                prevFocus = document.activeElement as HTMLElement;
+                element.focus();
+            }
+        }
+    })
+
+
     function selectSecurity(index: number){
-        securityId.set(securities[index].securityId);
+        chartQuery.update((v:ChartQuery) => {
+            v.securityId =  (securities[index].securityId);
+            return v
+        })
         closePopup();
     }
 
-    function getSecurities(event: KeyboardEvent){
-        if (event.key === "Enter"){
-            if (securities.length > 0) {
-                selectSecurity(selectedSecurityIndex);
-            }
-        } else if (event.key === "Escape") {
-            closePopup();
-        } else {
-            privateRequest<Security[]>("getSecuritiesFromTicker",{ticker:ticker})
-            .then((result: Security[]) => securities = result)
-            .catch((error: string) => {});
+    function closePopup() {
+        instanceInputVisible.set(false)
+        securities = [];
+        inputString = "";
+        console.log(inputString)
+        inputType = "";
+        if (prevFocus){
+            prevFocus.focus()
         }
     }
 
-    function openPopup() {
-        isOpen = true;
-    }
-
-    function closePopup() {
-        isOpen = false;
-        securities = [];
+    function classifyInput(input){
+        if (input) {
+            return /^[0-9]$/.test(input[0]) ? "interval" : "ticker";
+        }else{
+            return null;
+        }
     }
 
     onMount(() => {
-        document.addEventListener('keydown', (event) => {
-            if (event.key === 'Escape') {
-                closePopup();
-            } else if (event.key === 'Enter' && isOpen) {
-                if (securities.length > 0) {
-                    selectSecurity(selectedSecurityIndex);
+        document.addEventListener('keydown',  (event) => {
+            if (get(instanceInputVisible)){
+                if (event.key === 'Escape') {
+                    closePopup();
+                }else if (event.key === 'Enter') {
+                    if (Array.isArray(securities) && securities.length > 0) {
+                        selectSecurity(0);
+                    }
+                    closePopup();
+                }else {
+                    if (/^[a-zA-Z0-9]$/.test(event.key.toLowerCase())) {
+                        inputString += event.key;
+                        console.log(inputString)
+                    }else if (event.key == "Backspace") {
+                        inputString = inputString.slice(0, -1);
+                    }
+                    inputType = classifyInput(inputString)
+                    if(inputType === "ticker") {
+                        privateRequest<Security[]>("getSecuritiesFromTicker",{ticker:inputString})
+                        .then((result: Security[]) => securities = result)
+                        .catch((error: string) => {});
+                    }
                 }
             }
         });
     });
 
 </script>
-{#if isOpen}
+{#if $instanceInputVisible}
     <div class="popup">
+    <div>{inputString}</div>
+    <div>{inputType}</div>
         <table>
-            {#if Array.isArray(securities)}
+            {#if Array.isArray(securities) && securities.length > 0}
             {#each securities as sec, i}
                 <tr class={selectedSecurityIndex === i ? 'selected' : ''} on:click={() => selectSecurity(i)}> 
                     <td>{sec.ticker}</td>
@@ -76,14 +103,22 @@
 
 
 <style>
+
     .popup {
-        display: block;
-        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
         background-color: white;
         border: 1px solid #ccc;
         z-index: 1000;
-        padding: 10px;
+        padding: 20px;
         width: 300px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
     }
     .hidden {
         display: none;
