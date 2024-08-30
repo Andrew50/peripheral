32187b0215a2679c72f325a673ad26ef344b7548
bliuak <cs.benliu@gmail.com>
2024-08-30 02:31:22 -0400

    f

---
+++ backend/tasks/stockdata.go
@@ -0,0 +1,43 @@
+package tasks
+
+import (
+	"api/data"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type GetChartDataArgs struct {
+	Ticker    string `json:"ticker"`
+	Timeframe string `json:"timeframe"`
+	//StartTime string `json:"starttime"`
+	//EndTime   string `json:"endtime"`
+}
+type GetChartDataResults struct {
+	Datetime string  `json:"time"`
+	Open     float64 `json:"open"`
+	High     float64 `json:"high"`
+	Low      float64 `json:"low"`
+	Close    float64 `json:"close"`
+}
+
+func GetChartData(conn *data.Conn, userId int, rawArgs json.RawMessage) (interface{}, error) {
+	var args GetChartDataArgs
+	if err := json.Unmarshal(rawArgs, &args); err != nil {
+		return nil, fmt.Errorf("getChartData invalid args: %v", err)
+	}
+	iter := data.GetAggsData(conn.Polygon, args.Ticker, 1, "day", data.MillisFromDatetimeString("2023-01-01"),
+		data.MillisFromDatetimeString("2024-08-29"), 1000)
+	var barDataList []GetChartDataResults
+	for iter.Next() {
+		var barData GetChartDataResults
+		barData.Datetime = time.Time(iter.Item().Timestamp).Format(time.DateOnly)
+		barData.Open = iter.Item().Open
+		barData.High = iter.Item().High
+		barData.Low = iter.Item().Low
+		barData.Close = iter.Item().Close
+
+		barDataList = append(barDataList, barData)
+	}
+	return barDataList, nil
+}
--- frontend/src/routes/app/chart.svelte
+++ frontend/src/routes/app/chart.svelte
@@ -1,31 +1,77 @@
 <script lang="ts">
-import { createChart, ColorType } from 'lightweight-charts';
+import { createChart, ColorType} from 'lightweight-charts';
+import {privateRequest} from '../../store';
+import type {IChartApi, ISeriesApi, CandlestickData, Time, WhitespaceData, CandlestickSeriesOptions, DeepPartial, CandlestickStyleOptions, SeriesOptionsCommon} from 'lightweight-charts';
 import { onMount } from 'svelte';
-function Chart() {
-    console.log("Working")
+let mainChart: IChartApi;
+let mainChartCandleSeries: ISeriesApi<"Candlestick", Time, WhitespaceData<Time> | CandlestickData<Time>, CandlestickSeriesOptions, DeepPartial<CandlestickStyleOptions & SeriesOptionsCommon>>
+let currentTicker: string;
+let currentTimeframe: string; 
+interface barData {
+    time: string;
+    open: number; 
+    high: number;
+    low: number;
+    close: number;
+}
+function initializeChart()  {
     const chartOptions = { layout: { textColor: 'black', background: { type: ColorType.Solid, color: 'white' } } };
     const chartContainer = document.getElementById('chart_container');
     if (chartContainer) {
-        const chart = createChart(chartContainer, chartOptions);
+        mainChart = createChart(chartContainer, chartOptions);
 
-        const candleSeries = chart.addCandlestickSeries({
+        mainChartCandleSeries = mainChart.addCandlestickSeries({
             upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
             wickUpColor: '#26a69a', wickDownColor: '#ef5350',
         });
-        candleSeries.setData([
+        const candleData = [
             {time: "2024-08-12", open: 1, high: 2, low: 0.5, close: 2},
             {time: "2024-08-13", open: 1, high: 2, low: 0.5, close: 2},
             {time: "2024-08-14", open: 1, high: 2, low: 0.5, close: 2},
             {time: "2024-08-15", open: 1, high: 2, low: 0.5, close: 2},
             {time: "2024-08-16", open: 1, high: 2, low: 0.5, close: 2},
-        ]);
-        chart.timeScale().fitContent();
+        ]
+
+        mainChartCandleSeries.setData(candleData);
+        mainChart.timeScale().fitContent();
     }
 
 
 }
+function updateChart(chart: IChartApi) {
+    console.log(currentTicker)
+    let barDataList: barData[] = []
+    privateRequest<barData[]>("getChartData", {ticker:currentTicker, timeframe:currentTimeframe})
+        .then((result: barData[]) => {
+            barDataList = result;
+
+            let newData = [];
+            for (let i =0; i < barDataList.length; i++) {
+                newData.push({
+                    time: barDataList[i].time, 
+                    open: barDataList[i].open, 
+                    high: barDataList[i].high, 
+                    low: barDataList[i].low,
+                    close: barDataList[i].close
+                });
+            }
+           
+            console.log(newData)
+            mainChart.removeSeries(mainChartCandleSeries)
+            mainChartCandleSeries = mainChart.addCandlestickSeries({
+                    upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+                    wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+                })
+            mainChartCandleSeries.setData(newData)
+            mainChart.timeScale().fitContent();
+        })
+        .catch((error: string) => {
+            console.error("Error fetching chart data:", error);
+        });
+
+}
     onMount(() => {
-    Chart(); // Optionally call Chart function on component mount
+    initializeChart(); // Optionally call Chart function on component mount
   });
 </script>
 <style>
@@ -35,5 +81,7 @@ function Chart() {
     }
   </style>
 <p> Lightweight Charts</p>
-<button on:click={Chart}>Test</button>
+<input bind:value={currentTicker} placeholder="ticker"/>
+<input bind:value={currentTimeframe} placeholder="timeframe"/>
+<button on:click={() => updateChart(mainChart)}>Get Data</button>
 <div id="chart_container"></div>
