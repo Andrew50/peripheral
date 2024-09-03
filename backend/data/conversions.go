d228bc7a51fe9c1c038ed0a7f6a57fa7201c3c41
bliuak <cs.benliu@gmail.com>
2024-09-02 21:22:46 -0400

    f

--- backend/data/conversions.go
+++ backend/data/conversions.go
@@ -1,11 +1,32 @@
 package data
+
 import (
+	"errors"
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/polygon-io/client-go/rest/models"
 )
 
 func StringToTime(datetimeStr string) (time.Time, error) {
-	var layout string
+	layouts := []string{
+		time.DateTime,
+		time.DateOnly,
+	}
+	for _, layout := range layouts {
+		if dt, err := time.Parse(layout, datetimeStr); err == nil {
+			easternTimeLocation, tzErr := time.LoadLocation("America/New_York")
+			if tzErr != nil {
+				return time.Time{}, fmt.Errorf("Failed to load EST timezone: %w", err)
+			}
+			return dt.In(easternTimeLocation), nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("unsupported datetime format: %s", datetimeStr)
+
+	// aj old code
+	/*var layout string
 
 	if len(datetimeStr) == len("2023-01-01") {
 		layout = "2006-01-02" // Date format
@@ -20,5 +41,41 @@ func StringToTime(datetimeStr string) (time.Time, error) {
 		return time.Time{}, fmt.Errorf("failed to parse datetime: %w", err)
 	}
 
-	return parsedTime, nil
+	return parsedTime, nil*/
+
+}
+func MillisFromDatetimeString(datetime string) models.Millis {
+	layouts := []string{
+		time.DateTime,
+		time.DateOnly,
+	}
+	for _, layout := range layouts {
+		if dt, err := time.Parse(layout, datetime); err == nil {
+			easternTimeLocation, tzErr := time.LoadLocation("America/New_York")
+			if tzErr != nil {
+				log.Fatal(tzErr)
+			}
+			return models.Millis(dt.In(easternTimeLocation))
+		}
+	}
+	log.Fatal(errors.New("invalid datetime string"))
+	return models.Millis(time.Now())
+
+}
+func NanosFromDatetimeString(datetime string) models.Nanos {
+	layouts := []string{
+		time.RFC3339Nano,
+		time.DateTime,
+	}
+	for _, layout := range layouts {
+		if dt, err := time.Parse(layout, datetime); err == nil {
+			easternTimeLocation, tzErr := time.LoadLocation("America/New_York")
+			if tzErr != nil {
+				fmt.Print("eastern timezone error")
+			}
+			return models.Nanos(dt.In(easternTimeLocation))
+		}
+	}
+	log.Fatal(errors.New("invalid datetime string"))
+	return models.Nanos(time.Now())
 }
--- backend/data/polyRest.go
+++ backend/data/polyRest.go
@@ -3,7 +3,6 @@ package data
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -212,41 +211,6 @@ func GetTickerEvents(client *polygon.Client, id string) ([]models.TickerEventRes
 	}
 	return res.Results, nil
 
-}
-func MillisFromDatetimeString(datetime string) models.Millis {
-	layouts := []string{
-		time.DateTime,
-		time.DateOnly,
-	}
-	for _, layout := range layouts {
-		if dt, err := time.Parse(layout, datetime); err == nil {
-			easternTimeLocation, tzErr := time.LoadLocation("America/New_York")
-			if tzErr != nil {
-				log.Fatal(tzErr)
-			}
-			return models.Millis(dt.In(easternTimeLocation))
-		}
-	}
-	log.Fatal(errors.New("invalid datetime string"))
-	return models.Millis(time.Now())
-
-}
-func NanosFromDatetimeString(datetime string) models.Nanos {
-	layouts := []string{
-		time.RFC3339Nano,
-		time.DateTime,
-	}
-	for _, layout := range layouts {
-		if dt, err := time.Parse(layout, datetime); err == nil {
-			easternTimeLocation, tzErr := time.LoadLocation("America/New_York")
-			if tzErr != nil {
-				fmt.Print("eastern timezone error")
-			}
-			return models.Nanos(dt.In(easternTimeLocation))
-		}
-	}
-	log.Fatal(errors.New("invalid datetime string"))
-	return models.Nanos(time.Now())
 }
 
 type TickerResponse struct {
