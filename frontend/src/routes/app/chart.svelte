c733495713e5ae28f9e3a22a268cf6a8f429984a
bliuak <cs.benliu@gmail.com>
2024-09-02 22:57:03 -0400

    k

--- frontend/src/routes/app/chart.svelte
+++ frontend/src/routes/app/chart.svelte
@@ -120,7 +120,7 @@
             if (timeframe && timeframe.length < 1){
                 return
             }
-            privateRequest<barData[]>("getChartData", {securityId:v.securityId, timeframe:v.timeframe, datetime:v.datetime, bars:100})
+            privateRequest<barData[]>("getChartData", {securityId:v.securityId, timeframe:v.timeframe, datetime:v.datetime, direction:"backward", numbars:100, extendedhours:false})
             .then((result: barData[]) => {
                 if (! (Array.isArray(result) && result.length > 0)){ return}
                 barDataList = result;
