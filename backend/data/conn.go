b63701f03ebab7ccd41d37469d39b357874f04ae
bliuak <cs.benliu@gmail.com>
2024-09-12 17:07:38 -0400

    ff

--- backend/data/conn.go
+++ backend/data/conn.go
@@ -1,23 +1,27 @@
 package data
 
 import (
-	polygon "github.com/polygon-io/client-go/rest"
 	"context"
+	"fmt"
 	"log"
 	"time"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	polygonws "github.com/polygon-io/client-go/websocket"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Conn struct {
 	//Cache *redis.Client
-	DB      *pgxpool.Pool
-	Polygon *polygon.Client
+	DB        *pgxpool.Pool
+	Polygon   *polygon.Client
+	PolygonWS *polygonws.Client
 }
 
 func InitConn(inContainer bool) (*Conn, func()) {
 	//TODO change this sahit to use env vars as well
-    var dbUrl string
+	var dbUrl string
 	if inContainer {
 		dbUrl = "postgres://postgres:pass@db:5432"
 	} else {
@@ -29,24 +33,36 @@ func InitConn(inContainer bool) (*Conn, func()) {
 		dbConn, err = pgxpool.Connect(context.Background(), dbUrl)
 		if err != nil {
 			//if strings.Contains(err.Error(), "the database system is starting up") {
-            log.Println("waiting for db")
-            time.Sleep(5 * time.Second)
+			log.Println("waiting for db")
+			time.Sleep(5 * time.Second)
 		} else {
 			break
 		}
 	}
 	/*
-	   cache_url :="redis:6379"
-	   cache := redis.NewClient(&redis.Options{Addr: cache_url,})
-	   err = cache.Ping(context.Background()).Err()
-	   if err != nil {
-           //god
-	   }
+			   cache_url :="redis:6379"
+			   cache := redis.NewClient(&redis.Options{Addr: cache_url,})
+			   err = cache.Ping(context.Background()).Err()
+			   if err != nil {
+		           //god
+			   }
 	*/
 	//return &Conn{DB: db, Cache: cache}
 	//polygonConn := polygon.New(os.Getenv("POLYGON_API_KEY"))
 	polygonConn := polygon.New("ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm")
-	conn := &Conn{DB: dbConn, Polygon: polygonConn}
+	polygonWSConn, err := polygonws.New(polygonws.Config{
+		APIKey: "ogaqqkwU1pCi_x5fl97pGAyWtdhVLJYm",
+		Feed:   polygonws.RealTime,
+		Market: polygonws.Stocks,
+	})
+	if err != nil {
+		fmt.Printf("Error init polygonWs connection")
+	}
+	defer polygonWSConn.Close()
+	if err := polygonWSConn.Connect(); err != nil {
+		fmt.Printf("Error connecting to polygonWS")
+	}
+	conn := &Conn{DB: dbConn, Polygon: polygonConn, PolygonWS: polygonWSConn}
 
 	cleanup := func() {
 		conn.DB.Close()
--- backend/go.mod
+++ backend/go.mod
@@ -7,15 +7,18 @@ require (
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/lib/pq v1.10.9
 	github.com/polygon-io/client-go v1.16.5
+	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/form/v4 v4.2.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/go-resty/resty/v2 v2.13.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -26,7 +29,9 @@ require (
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637 // indirect
 )
