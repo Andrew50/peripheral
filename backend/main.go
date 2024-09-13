a5d837d41ed23be8b00369ba21f1e730a28ae73d
bliuak <cs.benliu@gmail.com>
2024-09-12 22:44:07 -0400

    ffff

--- backend/main.go
+++ backend/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"backend/data"
 	"backend/server"
-    "backend/utils"
+	"fmt"
 	"os"
-    "fmt"
 )
 
 func main() {
 	args := os.Args
+	if len(args) > 2 {
+		conn, close := utils.InitConn(false)
+		defer close()
+		utils.PolygonDataToRedis(conn)
+	}
 	if len(args) > 1 {
-        fmt.Println("testing")
+		//test func
 		conn, close := utils.InitConn(false)
 		defer close()
 		err := data.InitTickerDatabase(conn)
-        fmt.Printf("ERROR: %v",err)
+		fmt.Printf("ERROR: %v", err)
 		if err != nil {
 			panic(err)
 		}
