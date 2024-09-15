caf03a69bb2e7a8abe577618183e50c91f4778d9
bliuak <cs.benliu@gmail.com>
2024-09-15 14:54:11 -0400

    ff

--- frontend/src/lib/utils/replayManager.ts
+++ frontend/src/lib/utils/replayManager.ts
@@ -3,7 +3,7 @@ import { privateRequest } from "$lib/core/backend";
 import type { TradeData } from "$lib/features/chart/interface";
 
 let replayStatus : boolean = false; 
-let globalCurrentTimestamp : number = 0; 
+let globalCurrentTimestamp : number = 0; // 0 means realTime 
 
 function beginReplay(timestamp : number) {
     replayStatus = true; 
