448536d673565ebc1934544e046ca4dcfd93194d
server <billingsandrewjohn@gmail.com>
2024-09-17 22:34:46 -0400

    working on watchlist backend

--- frontend/src/lib/features/chart/indicators.ts
+++ frontend/src/lib/features/chart/indicators.ts
@@ -14,3 +14,21 @@ export function calculateSMA(data: CandlestickData[], period: number): { time: U
     }
     return smaData;
 }
+
+export function calculateADR(data: CandlestickData[],period: number): {time: UTCTimestamp, value: number}[] {
+    let adrData: {time:UTCTimestamp, value: number}[] = [];
+    for (let i = 0; i < data.length; i ++) {
+        if (i >= period - 1){
+            let sum = 0;
+            for (let j = 0; j < period; j++) {
+                sum += (( data[i-j].high / data[i-j].low - 1) * 100)
+            }
+            const average = sum / period
+            adrData.push({time:data[i].time,value:average});
+        }
+    }
+    return adrData
+}
+
+
+export function calculateRVOL(data: CandlestickData[],volume:VolumeData){}
