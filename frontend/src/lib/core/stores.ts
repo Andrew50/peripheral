295eb4c17acb63c6dc56c16235b286e9937e0f9c
bliuak <cs.benliu@gmail.com>
2024-09-18 14:29:36 -0400

    f

--- frontend/src/lib/core/stores.ts
+++ frontend/src/lib/core/stores.ts
@@ -2,6 +2,7 @@ import type {Setup} from '$lib/core/types'
 import {writable} from 'svelte/store'
 import type {Writable} from 'svelte/store'
 import {privateRequest} from '$lib/core/backend'
+import { onMount } from 'svelte';
 
 privateRequest<Setup[]>('getSetups', {})
 .then((v: Setup[]) => {
@@ -16,3 +17,17 @@ privateRequest<Setup[]>('getSetups', {})
     console.error('Error fetching setups:', error);
 });
 export let setups: Writable<Setup[]> = writable([]);
+export let currentTimestamp = writable(0);
+
+
+export function updateTime() {
+
+    // ADD IF REPLAY 
+    currentTimestamp.set(Date.now()); 
+}
+
+
+export function formatTimestamp(timestamp : number) {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString('en-US') + ' ' + date.toLocaleTimeString('en-US');
+}
