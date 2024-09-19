baf8eee943b33c83c4b7ff39a29e40169628db87
server <billingsandrewjohn@gmail.com>
2024-09-18 21:55:00 -0400

    added adr but its fucked up, about to implement multi chart so wont be stable for a while

--- frontend/src/lib/core/stores.ts
+++ frontend/src/lib/core/stores.ts
@@ -9,40 +9,42 @@ export let watchlists: Writable<Watchlist[]> = writable([]);
 export let flagWatchlistId: number | undefined;
 export let flagWatchlist: Writable<Watch[]>
 export let currentTimestamp = writable(0);
-privateRequest<Setup[]>('getSetups', {})
-.then((v: Setup[]) => {
-    v = v.map((v:Setup) => {
-        return {...v,
-          activeScreen: true}
+export function initStores(){
+    privateRequest<Setup[]>('getSetups', {})
+    .then((v: Setup[]) => {
+        v = v.map((v:Setup) => {
+            return {...v,
+              activeScreen: true}
+        })
+        console.log(v)
+        setups.set(v);
     })
-    console.log(v)
-    setups.set(v);
-})
-.catch((error) => {
-    console.error('Error fetching setups:', error);
-});
+    .catch((error) => {
+        console.error('Error fetching setups:', error);
+    });
 
-function loadFlagWatchlist(){
-    privateRequest<Watch[]>("getWatchlistItems",{watchlistId:flagWatchlistId})
-    .then((v:Watch[])=>{
-        flagWatchlist = writable(v)
-    })
-}
-
-
-privateRequest<Watchlist[]>("getWatchlists",{})
-.then((list:Watchlist[])=>{
-    watchlists.set(list)
-    flagWatchlistId = list?.find((v:Watchlist)=>v.watchlistName === "flag").watchlistId
-    if (!flagWatchlistId){
-        privateRequest<number>("newWatchlist",{watchlistName:"flag"}).then((v:number)=>{
-            flagWatchlistId = v
-            loadFlagWatchlist()
+    function loadFlagWatchlist(){
+        privateRequest<Watch[]>("getWatchlistItems",{watchlistId:flagWatchlistId})
+        .then((v:Watch[])=>{
+            flagWatchlist = writable(v)
         })
     }
-    loadFlagWatchlist()
 
-})
+
+    privateRequest<Watchlist[]>("getWatchlists",{})
+    .then((list:Watchlist[])=>{
+        watchlists.set(list)
+        flagWatchlistId = list?.find((v:Watchlist)=>v.watchlistName === "flag").watchlistId
+        if (!flagWatchlistId){
+            privateRequest<number>("newWatchlist",{watchlistName:"flag"}).then((v:number)=>{
+                flagWatchlistId = v
+                loadFlagWatchlist()
+            })
+        }
+        loadFlagWatchlist()
+
+    })
+}
 
 
 
