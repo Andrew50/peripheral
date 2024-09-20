11fbd452a70fdbd3808e53e59af44509b698f57b
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-09-20 18:06:36 -0400

    watchlists are pretty robust

--- frontend/src/lib/features/watchlist.svelte
+++ frontend/src/lib/features/watchlist.svelte
@@ -13,13 +13,14 @@
 
     let newWatchlistName="";
     let currentWatchlistId: number;
-
-
+    let container: HTMLDivElement;
     onMount(()=>{
         selectWatchlist(flagWatchlistId)
     })
 
-
+    $: if (container){
+        container.addEventListener("keydown",handleKey)
+    }
     function addInstance(){
         const inst = {ticker:"",timestamp:0}
         queryInstanceInput(["ticker"],inst).then((i:Instance)=>{
@@ -33,7 +34,7 @@
                         if (empty){
                             return [i]
                         }else{
-                            return [i,...v]
+                            return [...v,i]
                         }
                     })
                 })
@@ -42,6 +43,12 @@
                 addInstance()
             },10)
         })
+    }
+
+    function handleKey(event: KeyboardEvent) {
+        if (event.key == "Tab" || /^[a-zA-Z0-9]$/.test(event.key.toLowerCase())) {
+            addInstance()
+        }
     }
 
     function newWatchlist(){
@@ -78,51 +85,118 @@
             activeList.set(v)
         })
     }
+    function deleteWatchlist(id:number){
+        privateRequest<void>("deleteWatchlist",{watchlistId:id})
+        .then(()=>{
+            watchlists.update((v:Watchlist[])=>{
+                return v.filter((v:Watchlist)=>v.watchlistId !== id)
+            })
+            if (id === flagWatchlistId){
+                flagWatchlist.set([])
+            }
+        })
+    }
 
 </script>
 
+<div tabindex="-1" class="container" bind:this={container}>
+    <div  class="buttons-container">
 
-<div class="buttons-container">
-
-{#if Array.isArray($watchlists)}
-    <select id="watchlists" bind:value={currentWatchlistId} on:change={(event) => selectWatchlist(event.target.value)}>
-        {#each $watchlists as watchlist}
-            <option value={watchlist.watchlistId}>
-                {watchlist.watchlistName}
-            </option>
-        {/each}
-    </select>
+    {#if Array.isArray($watchlists)}
+      <div class="watchlist-container">
+        <select id="watchlists" bind:value={currentWatchlistId} on:change={(event) => selectWatchlist(event.target.value)}>
+            {#each $watchlists as watchlist}
+                <option value={watchlist.watchlistId}>
+                    {watchlist.watchlistName}
+                </option>
+            {/each}
+        </select>
+            <button class="button delete-watchlist" on:click={() => deleteWatchlist(currentWatchlistId)}>x</button>
+      </div>
     {/if}
     <button class="button" on:click={newWatchlist}>New</button>
-    <input bind:value={newWatchlistName} placeholder="Name"/>
-    <button class="button" on:click={addInstance}> Add </button>
-</div>
-<List parentDelete={deleteItem} columns={["ticker", "change"]} list={activeList}/>
+    <input class="input" bind:value={newWatchlistName} placeholder="Name"/>
+    </div>
 
+    <List parentDelete={deleteItem} columns={["ticker", "change"]} list={activeList}/>
+</div>
 
 <style>
   @import "$lib/core/colors.css";
+
+    .container {
+        outline: none;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width:100%;
+        justify-content: flex-start;
+    }
   .buttons-container {
     display: flex;
     flex-wrap: wrap;
     gap: 10px;
     margin-bottom: 20px;
-}
-    .button {
-        padding: 10px 20px;
-        background-color: var(--c2);
-        border: 1px solid var(--c4);
-        border-radius: 8px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-        font-size: 16px;
-        display: inline-block;
-    }
+  }
 
-    .button:hover {
-        background-color: var(--c3-hover);
-    }
+  .button {
+    padding: 10px 20px;
+    background-color: var(--c2);
+    border: 1px solid var(--c4);
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    font-size: 16px;
+  }
+  .input {
+    padding: 10px 20px;
+    background-color: var(--c2);
+    border: 1px solid var(--c4);
+    border-radius: 8px;
+    cursor: pointer;
+    font-size: 16px;
+
+  }
+
+  .button:hover {
+    background-color: var(--c3-hover);
+  }
+
+  /* Style for select dropdown */
+  .watchlist-container {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  select {
+    padding: 10px;
+    background-color: var(--c1);
+    color: var(--f1);
+    border: 1px solid var(--c4);
+    border-radius: 8px;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+  }
+
+  select:hover {
+    background-color: var(--c2);
+  }
+
+  /* Style for delete watchlist button */
+  .delete-watchlist {
+    background-color: var(--c3);
+    color: var(--f1);
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 8px;
+    font-size: 16px;
+  }
+
+  .delete-watchlist:hover {
+    background-color: var(--c3-hover);
+  }
+  
 </style>
-
-
-
