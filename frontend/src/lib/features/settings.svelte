377727b95b5210eebf7cf513a19665e104a01d18
test@example.com   Andrew Billings <billingsandrewjohn@gmail.com>
2024-09-20 21:23:54 -0400

    settings

--- frontend/src/lib/features/settings.svelte
+++ frontend/src/lib/features/settings.svelte
@@ -1,13 +1,13 @@
 <script lang='ts'>
-    import { chartLayout } from '$lib/core/stores';
+    import { settings } from '$lib/core/stores';
     import { get } from 'svelte/store';
-    let rows: number = get(chartLayout).rows;
-    let columns: number = get(chartLayout).columns;
+    import type {Settings} from '$lib/core/types'
     let errorMessage: string = '';
+    let tempSettings:Settings=get(settings)
 
     function updateLayout() {
-        if (rows > 0 && columns > 0) {
-            chartLayout.set({ rows, columns });
+        if (tempSettings.rows > 0 && tempSettings.columns > 0) {
+            settings.set(tempSettings)
             errorMessage = '';
         } else {
             errorMessage = 'Please enter valid numbers greater than 0 for both rows and columns.';
@@ -23,22 +23,31 @@
 
 <div>
     <div>
-        <label for="rows">Chart Rows:</label>
+        <label for="chart rows">Chart Rows:</label>
         <input 
             type="number" 
             id="rows" 
-            bind:value={rows} 
+            bind:value={tempSettings.chartRows} 
             min="1" 
             on:keypress={handleKeyPress} 
         />
     </div>
     <div>
-        <label for="columns">Chart Columns:</label>
+        <label for="chart columns">Chart Columns:</label>
         <input 
             type="number" 
             id="columns" 
-            bind:value={columns} 
+            bind:value={tempSettings.chartColumns} 
             min="1" 
+            on:keypress={handleKeyPress} 
+        />
+    </div>
+    <div>
+        <label for="dollar volume">Chart Columns:</label>
+        <input 
+            type="button" 
+            id="dolvol" 
+            bind:value={tempSettings.dolvol} 
             on:keypress={handleKeyPress} 
         />
     </div>
