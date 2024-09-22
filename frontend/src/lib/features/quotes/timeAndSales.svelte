180629c3054779079b5c2d5946f16c4367706c51
server <billingsandrewjohn@gmail.com>
2024-09-22 18:21:53 -0400

    backend after hours filtering, have to figure out hwo to aggregate, time and sales shows exchange now

--- frontend/src/lib/features/quotes/timeAndSales.svelte
+++ frontend/src/lib/features/quotes/timeAndSales.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import { onDestroy } from 'svelte';
+    import { onDestroy, onMount } from 'svelte';
     import type { Writable } from 'svelte/store';
     import type { TradeData, QuoteData ,Instance} from '$lib/core/types';
     import { getStream } from '$lib/utils/stream';
     import '$lib/core/global.css'
     import {settings} from '$lib/core/stores'
-    import type{Settings} from '$lib/core/stores'
+    import {get} from 'svelte/store'
+    import {privateRequest} from "$lib/core/backend"
     export let instance: Writable<Instance>;
     let store: Writable<TradeData>;
     let quoteStore: Writable<QuoteData>;
@@ -15,15 +16,22 @@
     let unsubscribeQuote: Function = () => {};
     interface TaS extends TradeData {
         color: string
+        exchangeName: string
     }
+    type Exchanges = {[exchangeId:number]:string}
+    let exchanges: Exchanges = {}
     let allTrades: TaS[] = [];
     let currentBid = 0;
     let currentAsk = 0;
     const maxLength = 20;
     let prevSecId = -1;
-    let divideTaS = false
-    settings.subscribe((v:Settings)=>{
-        divideTaS = v.divideTaS
+    let divideTaS = get(settings).divideTaS
+    let filterTaS = get(settings).filterTaS
+    onMount(()=>{
+        privateRequest<Exchanges>("getExchanges",{})
+        .then((v:Exchanges)=>{
+            exchanges = v
+        })
     })
 
     instance.subscribe((instance: Instance) => {
@@ -41,10 +49,15 @@
         allTrades = []
         unsubscribeTrade = store.subscribe((newTrade: TradeData) => {
             if (newTrade.timestamp !== undefined && newTrade.timestamp !== 0) {
+                if(filterTaS){
+                    if (newTrade.size < 100) return;
+                }
+
                 if (divideTaS){
                     newTrade.size = Math.floor(newTrade.size / 100)
                 }
-                const newRow: TaS = {color:getPriceColor(newTrade.price),...newTrade}
+                const exchangeName = exchanges[newTrade.exchange]
+                const newRow: TaS = {color:getPriceColor(newTrade.price),...newTrade,exchangeName:exchangeName}
                 allTrades = [newRow,...allTrades].slice(0,maxLength);
             }
         });
@@ -85,7 +98,8 @@
                 <tr>
                     <th>Time</th>
                     <th>Price</th>
-                    <th>{settings.dividTaS?"Size" : "Size*100"}</th>
+                    <th>{$settings.divideTaS ? "Size*100":"Size"}</th>
+                    <th>Exchange</th>
                 </tr>
             </thead>
             <tbody>
@@ -94,10 +108,12 @@
                     <td>{new Date(trade.timestamp).toLocaleTimeString()}</td>
                     <td>{trade.price?.toFixed(3)}</td>
                     <td>{trade.size}</td>
+                    <td>{trade.exchangeName}</td>
                 </tr>
             {/each}
             {#each Array(maxLength - allTrades.length).fill(0) as _}
                 <tr>
+                    <td>&nbsp;</td>  <!-- Empty cell -->
                     <td>&nbsp;</td>  <!-- Empty cell -->
                     <td>&nbsp;</td>  <!-- Empty cell -->
                     <td>&nbsp;</td>  <!-- Empty cell -->
