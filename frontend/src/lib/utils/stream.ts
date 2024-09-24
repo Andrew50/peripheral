f726c01e4a178e948b29125f9ee87927c10d473e
bliuak <cs.benliu@gmail.com>
2024-09-23 20:47:17 -0400

    f

--- frontend/src/lib/utils/stream.ts
+++ frontend/src/lib/utils/stream.ts
@@ -3,14 +3,20 @@ import type { Instance,TradeData,QuoteData } from "$lib/core/types"
 import {RealtimeStream} from "$lib/utils/realtimeStream"
 import {ReplayStream} from "$lib/utils/replayStream"
 import type {Writable} from 'svelte/store'
-import {writable} from 'svelte/store'
+import {writable, get} from 'svelte/store'
 import {ESTSecondstoUTCSeconds} from '$lib/core/timestamp'
 import {privateRequest} from '$lib/core/backend'
 export type ChannelType = "fast" | "slow" | "quote" | "close"
 export type Channels = Map<string,{count:number,store:Writable<any>}>
 export let activeChannels: Channels = new Map()
-import {timeEvent,currentTimestamp} from '$lib/core/stores'
+import {timeEvent,currentTimestamp, replayInfo} from '$lib/core/stores'
+import type {ReplayInfo} from '$lib/core/stores'
 import type {TimeEvent} from '$lib/core/stores'
+
+import { getReferenceStartTimeForDateMilliseconds, isOutsideMarketHours } from '$lib/core/timestamp';
+import { chartQuery } from '$lib/features/chart/interface';
+import { ESTSecondstoUTCMillis } from '$lib/core/timestamp';
+
 const realtimeStream = new RealtimeStream;
 export const replayStream = new ReplayStream;
 let currentStream: RealtimeStream | ReplayStream = realtimeStream;
@@ -69,8 +75,12 @@ export function getStream<T extends TradeData|QuoteData|number>(instance:Instanc
 export function startReplay(timestamp : number){
     currentStream.stop()
     currentStream = replayStream
-    currentStream.start(timestamp)
-    currentTimestamp.set(timestamp)
+    var timestampToUse = timestamp
+    if(isOutsideMarketHours(timestampToUse)) {
+        timestampToUse = ESTSecondstoUTCMillis(getReferenceStartTimeForDateMilliseconds(timestampToUse, get(chartQuery).extendedHours)/1000)
+    }
+    currentStream.start(timestampToUse)
+    currentTimestamp.set(timestampToUse)
     timeEvent.update((v:TimeEvent)=>{
         v.event = "replay"
         return {...v}
@@ -87,6 +97,10 @@ export function resumeReplay() {
 }
 export function stopReplay(){
     currentStream.stop()
+    replayInfo.update((r:ReplayInfo) => {
+        r.status = "inactive";
+        return r
+    })
     currentStream = realtimeStream
     currentStream.start()
 }
