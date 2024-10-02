f9793814ee19fb9e010e00f8a0f416f11ea0e551
server <billingsandrewjohn@gmail.com>
2024-10-02 13:34:10 -0400

    sample adding from instance click, can also create new setup

---
+++ frontend/src/lib/features/setups/interface.ts
@@ -0,0 +1,21 @@
+import type {Instance} from '$lib/core/types'
+import {privateRequest} from '$lib/core/backend'
+import {writable} from 'svelte/store'
+import {dispatchMenuChange} from '$lib/core/stores'
+type SetupEvent = "new"|"save"|"cancel"
+export let eventDispatcher = writable<SetupEvent>()
+export function setSample (setupId:number|"new",instance:Instance):void{
+    if (setupId === "new"){
+        dispatchMenuChange.set("setups")
+        eventDispatcher.set("new")
+        const unsub = eventDispatcher.subscribe((v:SetupEvent)=>{
+            if (v === "new"){
+            }else if (v === "cancel"){
+                unsub()
+            }else{
+                privateRequest<void>("setSample",{setupId:v,...instance})
+                unsub()
+            }
+        })
+    }
+}
