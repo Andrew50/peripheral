5883d590fbab6b3ad8956684c865dc645100c32a
server <billingsandrewjohn@gmail.com>
2025-01-29 10:26:01 -0500

    seemingly working horizontal line functionality

--- db/rollouts.sql
+++ db/rollouts.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS horizontal_lines (
-    id SERIAL PRIMARY KEY,
-    userId INT REFERENCES users(userId) ON DELETE CASCADE,
-    securityId INT,
-    price FLOAT NOT NULL,
-    UNIQUE (userId, securityId, price)
-);
-CREATE INDEX IF NOT EXISTS idxUserIdSecurityIdPrice ON horizontal_lines(userId, securityId, price);
+/*CREATE TABLE IF NOT EXISTS horizontal_lines (
+ id SERIAL PRIMARY KEY,
+ userId INT REFERENCES users(userId) ON DELETE CASCADE,
+ securityId INT,
+ price FLOAT NOT NULL,
+ UNIQUE (userId, securityId, price)
+ );
+ CREATE INDEX IF NOT EXISTS idxUserIdSecurityIdPrice ON horizontal_lines(userId, securityId, price);
+ */
