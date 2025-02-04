d64cf52f2a2419824f56bed60968996321584274
bliuak <cs.benliu@gmail.com>
2025-02-04 15:05:45 -0500

    f

--- backend/alerts/algo.go
+++ backend/alerts/algo.go
@@ -1,10 +1,32 @@
 package alerts
 
 import (
-    "backend/utils"
+	"backend/utils"
+	"fmt"
+	"log"
+	"time"
 )
 
 func processAlgoAlert(conn *utils.Conn, alert Alert) error {
-    return nil
+	return nil
 }
+func processTapeBursts(conn *utils.Conn, alert Alert) {
+	socket.
+		alertAggDataMutex.RLock()
+	defer alertAggDataMutex.RUnlock()
+	for securityID, sd := range alertAggData {
+		if len(sd.VolBurstData.VolumeThreshold) == 0 {
+			continue
+		}
+		ticker, err := utils.GetTicker(conn, securityID, time.Now())
+		fmt.Printf("Running volburst on %s\n", ticker)
+		if isTapeBurst(sd) {
+			if err != nil {
+				log.Printf("Error getting ticker for security %d: %v", securityID, err)
+				continue
+			}
+			dispatchAlert(conn, alert)
+		}
+	}
 
+}
