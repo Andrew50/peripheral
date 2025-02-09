a49a6539e056167133ecb67e32e4c62087636984
server <billingsandrewjohn@gmail.com>
2025-02-09 12:16:27 -0500

    fixed input window missing inputs

--- db/rollouts.sql
+++ db/rollouts.sql
@@ -42,3 +42,49 @@ CREATE TABLE trade_executions (
 ALTER TABLE securities
 ADD COLUMN sector VARCHAR(100),
     ADD COLUMN industry VARCHAR(100);
+CREATE TABLE alerts (
+    alertId SERIAL PRIMARY KEY,
+    userId SERIAL REFERENCES users(userId) ON DELETE CASCADE,
+    active BOOLEAN NOT NULL DEFAULT false,
+    alertType VARCHAR(10) NOT NULL CHECK (alertType IN ('price', 'setup', 'algo')),
+    -- Restrict the allowed alert types
+    setupId INT REFERENCES setups(setupId) ON DELETE CASCADE,
+    --algoId INT REFERENCES algos(algoId) ON DELETE CASCADE,
+    algoId INT,
+    price DECIMAL(10, 4),
+    direction Boolean,
+    securityID INT,
+    CONSTRAINT chk_alert_price_or_setup CHECK (
+        (
+            alertType = 'price'
+            AND price IS NOT NULL
+            AND securityID IS NOT NULL
+            AND direction IS NOT NULL
+            AND algoId IS NULL
+            AND setupId IS NULL
+        )
+        OR (
+            alertType = 'setup'
+            AND setupId IS NOT NULL
+            AND algoId IS NULL
+            AND price IS NULL
+            AND securityID IS NULL
+        )
+        OR (
+            alertType = 'algo'
+            AND algoId IS NOT NULL
+            AND setupId IS NULL
+            AND price IS NULL
+        )
+    )
+);
+CREATE INDEX idxAlertByUserId on alerts(userId);
+CREATE TABLE alertLogs (
+    alertLogId serial primary key,
+    alertId serial references alerts(alertId) on delete cascade,
+    timestamp timestamp not null,
+    securityId INT,
+    --references sercurities
+    unique(alertId, timestamp, securityId)
+);
+CREATE INDEX idxAlertLogId on alertLogs(alertLogId);
