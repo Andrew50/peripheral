3090ff6ef74ce515c8479195f5499703e1497d13
Andrew Billings <billingsandrewjohn@gmail.com>
2025-02-10 10:51:36 -0500

    sync

--- frontend/src/lib/features/chart/chart.svelte
+++ frontend/src/lib/features/chart/chart.svelte
@@ -827,7 +827,7 @@
 		chartContainer.addEventListener('contextmenu', (event: MouseEvent) => {
 			event.preventDefault();
 			const timestamp = ESTSecondstoUTCMillis(latestCrosshairPositionTime);
-			const price = chartCandleSeries.coordinateToPrice(event.clientY) || 0;
+			const price = Math.round(chartCandleSeries.coordinateToPrice(event.clientY) * 100) / 100 || 0;
 			const ins: Instance = { ...currentChartInstance, timestamp: timestamp, price: price };
 			queryInstanceRightClick(event, ins, 'chart');
 		});
@@ -859,7 +859,7 @@
 				if ($streamInfo.replayActive) {
 					currentChartInstance.timestamp = 0;
 				}
-				queryInstanceInput('any','any', currentChartInstance)
+				queryInstanceInput('any', 'any', currentChartInstance)
 					.then((v: Instance) => {
 						currentChartInstance = v;
 						queryChart(v, true);
