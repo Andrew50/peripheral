20c42e94fe937f4e0f0ecda3a86be2dc27e05180
server <billingsandrewjohn@gmail.com>
2025-03-05 00:02:19 -0500

    Refactor backend code and improve naming conventions
    - Standardize variable naming in alerts, jobs, and socket packages
    - Fix variable name casing (e.g., userID, securityId)
    - Add descriptive comments for functions
    - Remove unnecessary comments and improve code readability
    - Enhance error handling and logging in job execution

--- services/backend/jobs/journals.go
+++ services/backend/jobs/journals.go
@@ -1,49 +1,47 @@
 package jobs
 
 import (
-    "time"
-    "backend/utils"
-    "fmt"
-    "context"
+	"backend/utils"
+	"context"
+	"fmt"
+	"time"
 )
 
 func pushJournals(conn *utils.Conn, year int, month time.Month, day int) {
-    currentDate := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
-    userIds, err := getAllUserIds(conn)
-    if err != nil {
-        fmt.Printf("Error fetching user ids: %v\n", err)
-        return
-    }
-    for _, userId := range userIds {
-        _, err := conn.DB.Exec(context.Background(),` INSERT INTO journals (timestamp, userId, entry)
+	currentDate := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+	userIDs, err := getAllUserIDs(conn)
+	if err != nil {
+		fmt.Printf("Error fetching user ids: %v\n", err)
+		return
+	}
+	for _, userID := range userIDs {
+		_, err := conn.DB.Exec(context.Background(), ` INSERT INTO journals (timestamp, userId, entry)
             VALUES ($1, $2, $3);
-        `, currentDate, userId, "{}") // Assuming an empty JSON object for 'entry'
-        if err != nil {
-            fmt.Printf(" 0b1q Error inserting journal for userId %d: %v\n", userId, err)
-        } else {
-            fmt.Printf("1niuv %d on %v\n", userId, currentDate)
-        }
-    }
+        `, currentDate, userID, "{}") // Assuming an empty JSON object for 'entry'
+		if err != nil {
+			fmt.Printf(" 0b1q Error inserting journal for userId %d: %v\n", userID, err)
+		} else {
+			fmt.Printf("1niuv %d on %v\n", userID, currentDate)
+		}
+	}
 }
 
-func getAllUserIds(conn *utils.Conn) ([]int, error) {
-    rows, err := conn.DB.Query(context.Background(),"SELECT userId FROM users")
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-    var userIds []int
-    for rows.Next() {
-        var userID int
-        if err := rows.Scan(&userId); err != nil {
-            return nil, err
-        }
-        userIds = append(userIds, userId)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, err
-    }
-    return userIds, nil
+func getAllUserIDs(conn *utils.Conn) ([]int, error) {
+	rows, err := conn.DB.Query(context.Background(), "SELECT userId FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var userIDs []int
+	for rows.Next() {
+		var userID int
+		if err := rows.Scan(&userID); err != nil {
+			return nil, err
+		}
+		userIDs = append(userIDs, userID)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return userIDs, nil
 }
-
-
