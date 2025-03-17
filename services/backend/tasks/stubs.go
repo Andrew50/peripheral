030d652c9098aad0e2513b89c5093ee42a1e7a58
bliuak <cs.benliu@gmail.com>
2025-03-17 18:54:33 -0400

    f

--- services/backend/tasks/stubs.go
+++
@@ -1,6 +0,0 @@
-// Package tasks provides task-related functionality
-package tasks
-
-// DEPRECATED: This file is no longer needed as all functions have been moved to tasks.go.
-// This file is kept for historical reference but contains no active code.
-// All task implementations should be in tasks.go to avoid function redeclarations.
