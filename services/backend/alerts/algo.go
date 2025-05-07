41391ac6f663a15c511a967ca6afc333abc12d75
bliuak <cs.benliu@gmail.com>
2025-05-07 00:05:45 -0400

    fix lint

--- services/backend/alerts/algo.go
+++ services/backend/alerts/algo.go
@@ -1,12 +1,8 @@
 package alerts
 
-import (
-	"backend/utils"
-)
-
+/*
 func processAlgoAlert(conn *utils.Conn, alert Alert) error {
-	if alert.AlgoID != nil && *alert.AlgoID == 0 {
-		processTapeBursts(conn, alert)
-	}
+
 	return nil
 }
+*/
