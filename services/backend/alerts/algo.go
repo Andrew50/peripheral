fb7f9b82561faee3e2996b000d86e0d7d66b4d4f
bliuak <cs.benliu@gmail.com>
2025-05-06 23:23:23 -0400

    depreciate volburst

--- services/backend/alerts/algo.go
+++ services/backend/alerts/algo.go
@@ -1,11 +1,7 @@
 package alerts
 
 import (
-	"backend/socket"
 	"backend/utils"
-	"fmt"
-	"log"
-	"time"
 )
 
 func processAlgoAlert(conn *utils.Conn, alert Alert) error {
@@ -13,24 +9,4 @@ func processAlgoAlert(conn *utils.Conn, alert Alert) error {
 		processTapeBursts(conn, alert)
 	}
 	return nil
-}
-func processTapeBursts(conn *utils.Conn, alert Alert) {
-	socket.AggDataMutex.RLock()
-	defer socket.AggDataMutex.RUnlock()
-	for securityID, sd := range socket.AggData {
-		if len(sd.VolBurstData.VolumeThreshold) == 0 {
-			continue
-		}
-		ticker, err := utils.GetTicker(conn, securityID, time.Now())
-		fmt.Printf("Running volburst on %s\n", ticker)
-		if isTapeBurst(sd) {
-			if err != nil {
-				log.Printf("Error getting ticker for security %d: %v", securityID, err)
-				continue
-			}
-			fmt.Printf("Tape burst detected on %s %v\n", ticker, alert)
-			//dispatchAlert(conn, alert)
-		}
-	}
-
 }
