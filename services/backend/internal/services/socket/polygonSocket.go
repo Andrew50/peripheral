a93828cd5afa005b8e0d3054099f87f00f70b04a
bliuak <cs.benliu@gmail.com>
2025-05-12 16:57:38 -0400

    Refactor GetLastTrade function to include atLeastFullLot parameter for improved trade retrieval logic. Update related calls in alerts and security helper functions. Enhance socket handling by implementing listener count management for channels.

--- services/backend/internal/services/socket/polygonSocket.go
+++ services/backend/internal/services/socket/polygonSocket.go
@@ -137,7 +137,9 @@ func StreamPolygonDataToRedis(conn *data.Conn, polygonWS *polygonws.Client) {
 			              alerts.appendAggregate(securityId,msg.Open,msg.High,msg.Low,msg.Close,msg.Volume)*/
 			case models.EquityTrade:
 				channelNameType := getChannelNameType(msg.Timestamp)
-				channelName := fmt.Sprintf("%d-fast-%s", securityId, channelNameType)
+				fastChannelName := fmt.Sprintf("%d-fast-%s", securityId, channelNameType)
+				allChannelName := fmt.Sprintf("%d-all", securityId)
+
 				data := TradeData{
 					//					Ticker:     msg.Symbol,
 					Price:      msg.Price,
@@ -145,21 +147,27 @@ func StreamPolygonDataToRedis(conn *data.Conn, polygonWS *polygonws.Client) {
 					Timestamp:  msg.Timestamp,
 					Conditions: msg.Conditions,
 					ExchangeID: msg.Exchange,
-					Channel:    channelName,
+					Channel:    fastChannelName,
+				}
+				//if alerts.IsAggsInitialized() {
+				if useAlerts {
+					appendTick(conn, securityId, data.Timestamp, data.Price, data.Size)
+				}
+				if !hasListeners(fastChannelName) && !hasListeners(allChannelName) {
+					break
 				}
 				jsonData, err := json.Marshal(data)
 				if err != nil {
 					fmt.Println("Error marshling JSON:", err)
 				}
-				broadcastToChannel(channelName, string(jsonData))
-				channelName = fmt.Sprintf("%d-all", securityId)
-				data.Channel = channelName
+				broadcastToChannel(fastChannelName, string(jsonData))
+				data.Channel = allChannelName
 				jsonData, err = json.Marshal(data)
 				if err != nil {
 					fmt.Println("Error marshling JSON:", err)
 				}
 				//conn.Cache.Publish(context.Background(), channelName, string(jsonData))
-				broadcastToChannel(channelName, string(jsonData))
+				broadcastToChannel(allChannelName, string(jsonData))
 				now := time.Now()
 				nextDispatchTimes.RLock()
 				nextDispatch, exists := nextDispatchTimes.times[msg.Symbol]
@@ -167,10 +175,6 @@ func StreamPolygonDataToRedis(conn *data.Conn, polygonWS *polygonws.Client) {
 				// Only append tick if aggregates are initialized
 				//fmt.Println("debug: alerts.IsAggsInitialized()", alerts.IsAggsInitialized())
 
-				//if alerts.IsAggsInitialized() {
-				if useAlerts {
-					appendTick(conn, securityId, data.Timestamp, data.Price, data.Size)
-				}
 				//}
 				if !exists || now.After(nextDispatch) {
 					slowChannelName := fmt.Sprintf("%d-slow-%s", securityId, channelNameType)
@@ -187,6 +191,9 @@ func StreamPolygonDataToRedis(conn *data.Conn, polygonWS *polygonws.Client) {
 				}
 			case models.EquityQuote:
 				channelName := fmt.Sprintf("%d-quote", securityId)
+				if !hasListeners(channelName) {
+					break
+				}
 				data := QuoteData{
 					Timestamp: msg.Timestamp,
 					BidPrice:  msg.BidPrice,
