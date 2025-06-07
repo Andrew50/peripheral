c266f205b43ed5c5cc32f0beba2d34d8c986ce52
bliuak <cs.benliu@gmail.com>
2025-06-06 22:33:04 -0800

    slowly trying to split these files up

---
+++ services/frontend/src/lib/components/input/utils/inputTypes.ts
@@ -0,0 +1,23 @@
+
+
+import type { Instance } from '$lib/utils/types/types';
+
+export const allKeys = ['ticker', 'timestamp', 'timeframe', 'extendedHours'] as const;
+export type InstanceAttributes = (typeof allKeys)[number];
+
+export interface InputQuery {
+    // 'inactive': no UI shown
+    // 'initializing' setting up event handlers
+    // 'active': window is open waiting for input
+    // 'complete': one field completed (may still be active if more required)
+    // 'cancelled': user cancelled via Escape
+    // 'shutdown': about to close and reset to inactive
+    status: 'inactive' | 'initializing' | 'active' | 'complete' | 'cancelled' | 'shutdown';
+    inputString: string;
+    inputType: string;
+    inputValid: boolean;
+    instance: Instance;
+    requiredKeys: InstanceAttributes[] | 'any';
+    possibleKeys: InstanceAttributes[];
+    securities?: Instance[];
+}
