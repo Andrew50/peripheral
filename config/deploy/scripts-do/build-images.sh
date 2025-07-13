edee3dbdb4a646cd7d95fd7de593a37421c4a941
NathanVosburg <nathanjvosburg@gmail.com>
2025-07-13 15:43:50 -0400

    fixed?

--- config/deploy/scripts-do/build-images.sh
+++ config/deploy/scripts-do/build-images.sh
@@ -49,8 +49,8 @@ build_service() {
     $build_args \
     -t "$DOCKER_USERNAME/$srv:${DOCKER_TAG}" \
     -f "$dockerfile" \
-    "services/$srv"
-  
+    "$context"
+
   echo "✅ $srv build completed"
 }
 
