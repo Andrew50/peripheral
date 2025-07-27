fa5ca8a56f88dbb1fc8e8c48d0d256deca39f4f4
NathanVosburg <nathanjvosburg@gmail.com>
2025-07-26 21:48:40 -0400

    added job to hash pswds

--- services/backend/cmd/server/main.go
+++ services/backend/cmd/server/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	conn, cleanup := data.InitConn(true)
 	defer cleanup()
-	// stopScheduler := server.StartScheduler(conn)
-	// defer close(stopScheduler)
+	stopScheduler := server.StartScheduler(conn)
+	defer close(stopScheduler)
 	server.StartServer(conn)
 }
