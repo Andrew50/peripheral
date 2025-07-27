92319bd7e3cc6ecabc7ecae130ef3f8f9a956d5f
NathanVosburg <nathanjvosburg@gmail.com>
2025-07-26 22:27:35 -0400

    updated the migration

--- services/db/migrations/85.sql
+++ services/db/migrations/85.sql
@@ -3,7 +3,7 @@ BEGIN;
 
 ALTER TABLE users
 ADD COLUMN verified BOOLEAN NOT NULL DEFAULT FALSE,
-ADD COLUMN otp_code VARCHAR(10),
+ADD COLUMN otp_code INTEGER,
 ADD COLUMN otp_expires_at TIMESTAMP;
 
 -- make sure to not deactivate current accounts
