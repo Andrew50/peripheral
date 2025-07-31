40dbbc1a7e6440992d2000925984eaac72270626
NathanVosburg <nathanjvosburg@gmail.com>
2025-07-30 23:43:48 -0400

    fix migraiton

---
+++ services/db/migrations/87.sql
@@ -0,0 +1,12 @@
+-- Migration: 087_email_verification
+-- Description: Adds columns for email verification of users
+
+BEGIN;
+
+ALTER TABLE users
+ADD COLUMN verified BOOLEAN NOT NULL DEFAULT FALSE,
+ADD COLUMN otp_code VARCHAR(10),
+ADD COLUMN otp_code INTEGER,
+ADD COLUMN otp_expires_at TIMESTAMP;
+
+COMMIT;
